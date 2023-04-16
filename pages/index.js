01106275e17233fbccb7c4e0a5965b4c9e491475
Forum Patel <forum180301@gmail.com>
2023-04-16 20:36:21 +0530

    final commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from "../services";
-
-// const posts = [
-//   { title: "React Testing", excerpt: "Learn React Testing" },
-//   { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
-// ];
+import { FeaturedPosts } from "../sections/index";
 
 export default function Home({ posts }) {
   return (
@@ -15,6 +11,7 @@ export default function Home({ posts }) {
           <title>CMS BLOG</title>
           <link rel="icon" href="/favicon.ico"></link>
         </Head>
+        <FeaturedPosts />
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="lg:col-span-8 col-span-1">
             {posts.map((post) => (
